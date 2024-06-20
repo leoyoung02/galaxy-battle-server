d66624ef436b58c3d51239518785087a3b97cd26
leoyoung02 <flarelight0125@gmail.com>
2024-06-20 17:06:02 +0500

    new packet architecture in progress

--- src/game/network/Packets.ts
+++ src/game/network/Packets.ts
@@ -1,23 +1,29 @@
-import { SignData } from "./Types";
+import { SearchGameData, SignData } from "./Types";
 
 export enum PackTitle {
-    // for lobby
-    sign = 'sign',
-    startSearchGame = 'startSearchGame', // request
-    stopSearchGame = 'stopSearchGame', // request
-    gameSearching = 'gameSearching', // status, update, info
-    duel = 'duel',
+  // for lobby
+  sign = 'sign',
+  startSearchGame = 'startSearchGame', // request
+  stopSearchGame = 'stopSearchGame', // request
+  gameSearching = 'gameSearching', // status, update, info
+  duel = 'duel',
 }
 
 interface BasePacket {
-    title: PackTitle;
+  title: PackTitle;
 }
 
 export interface SignPacket extends BasePacket {
-    title: PackTitle.sign;
-    data: SignData;
+  title: PackTitle.sign;
+  data: SignData;
+}
+
+export interface StartSearchGamePacket extends BasePacket {
+  title: PackTitle.startSearchGame;
+  data: SearchGameData;
 }
 
 export type Packet =
-    SignPacket;
-    // | other packet
+    SignPacket
+  | StartSearchGamePacket;
+// | other packets
